cae61abd9a1781ea2cb129c7938e48ab03c88e1b
SiddhantKapadne <ksiddhant46@gmail.com>
2020-07-13 22:10:01 +0530

    Updates

---
+++ precache-manifest.8d7e7b51d8915a7786eac124dd1316ff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19377f4b87278af09a97a8eeabaaa2f7",
+    "url": "/Covid-India/index.html"
+  },
+  {
+    "revision": "cb584bf0c46a5bd18bc3",
+    "url": "/Covid-India/static/css/main.82e28c96.chunk.css"
+  },
+  {
+    "revision": "703c81ee1da2c75d410a",
+    "url": "/Covid-India/static/js/2.694085d3.chunk.js"
+  },
+  {
+    "revision": "44e39e61fb6a0450f7c8014318774752",
+    "url": "/Covid-India/static/js/2.694085d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb584bf0c46a5bd18bc3",
+    "url": "/Covid-India/static/js/main.53702eef.chunk.js"
+  },
+  {
+    "revision": "1f96f8d2c6b2b98a4006",
+    "url": "/Covid-India/static/js/runtime-main.821d4c84.js"
+  }
+]);
